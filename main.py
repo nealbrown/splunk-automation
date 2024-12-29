c317b9ddf48cc32e1f7c4428e0cdede8dcba7887
Neal Brown <nealbrown@gmail.com>
2024-12-28 18:27:18 -0600

    Cannot break out multiple files due to sessionkey global var

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import typer, requests, config, os
+import typer, requests, config
 from rich import print
 from xml.dom import minidom
 from typing_extensions import Annotated
@@ -7,22 +7,8 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 app = typer.Typer()
-serverclasses_app = typer.Typer()
-app.add_typer(serverclasses_app, name="serverclasses")
-
-@serverclasses_app.command("get-serverclasses")
-def get_serverclasses( 
-    host: Annotated[
-        str, typer.Option(envvar="SPLUNK_HOST")] = "splunk-ds-1", 
-    ):
-    """
-    Output Existing Serverclasses
-    """
-    print(f"Retrieving Serverclasses on {host}")
-    r = requests.get("https://" + host + ":8089" + "/services/deployment/server/serverclasses",
-        headers = { 'Authorization': ('Splunk %s' %session_key)},
-        data={}, verify=False)
-    print("Serverclasses: " + r.text)
+serverclass_app = typer.Typer()
+app.add_typer(serverclass_app, name="serverclass")
 
 @app.callback(invoke_without_command=True)
 def main(
@@ -49,6 +35,21 @@ def main(
         raise typer.Exit(code=1)
     if ctx.invoked_subcommand is None:
         print("No further command specified. Session key will expire in 60 minutes.")
+
+@serverclass_app.command()
+def get_serverclasses( 
+    host: Annotated[
+        str, typer.Option(envvar="SPLUNK_HOST")] = "splunk-ds-1", 
+    ):
+    """
+    Output Existing Serverclasses
+    """
+    print(f"Retrieving Serverclasses on {host}")
+    r = requests.get("https://" + host + ":8089" + "/services/deployment/server/serverclasses",
+        headers = { 'Authorization': ('Splunk %s' %session_key)},
+        data={}, verify=False)
+    print("Serverclasses: " + r.text)
+
 
 if __name__ == "__main__":
     app()
